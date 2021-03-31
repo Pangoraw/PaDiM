112062b706ac51064704ea7fc45d61cb8619495b
Paul <paul.berg@univ-ubs.fr>
2021-03-31 18:08:02 +0200

    convert models for padim shared

--- examples/convert_model.py
+++ examples/convert_model.py
@@ -4,7 +4,7 @@ import pickle
 sys.path.append('./')
 sys.path.append('./deep_svdd/src/')
 
-from padim import PaDiM
+from padim import PaDiM, PaDiMShared
 
 PARAMS_PATH = sys.argv[1]
 
@@ -13,10 +13,16 @@ print(f"Converting {PARAMS_PATH}")
 with open(PARAMS_PATH, 'rb') as f:
     params = pickle.load(f)
 
-padim = PaDiM.from_residuals(*params, device="cpu")
+if 'shared' in PARAMS_PATH:
+    padim = PaDiMShared.from_residuals(*params, device="cpu")
 
-padim.covs = padim.covs.permute(2, 0, 1)
-padim.means = padim.means.permute(1, 0)
+    padim.cov = padim.cov.permute(1, 0)
+else:
+    padim = PaDiM.from_residuals(*params, device="cpu")
+
+    padim.covs = padim.covs.permute(2, 0, 1)
+    padim.means = padim.means.permute(1, 0)
+
 
 params = padim.get_residuals()
 with open(PARAMS_PATH, 'wb') as f:
